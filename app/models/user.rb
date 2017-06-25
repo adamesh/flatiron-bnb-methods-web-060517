d027f64f2429df0df7eb46832d2aa0c57736151c
adamesh <adameshh@gmail.com>
2017-06-25 08:47:38 -0400

    added a new user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,9 @@ class User < ActiveRecord::Base
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :guests, :through => :listings, :foreign_key => 'host_id'
-  # Fuck these associations V Can't get them right. 
-  # has_many :hosts, :through => :reservation, :foreign_key => 'guest_id'
-  # has_many :reviews, :through => :reservations, :foreign_key => 'host_id'
+  has_many :host_reviews, :through => :listings, :source => "reviews"
+  # Test for the one below doesn't pass, but I believe it's the correct association.
+  # Maybe needs a save methods somewhere?
+  has_many :hosts, :through => :listings, :foreign_key => 'guest_id'
 
 end
