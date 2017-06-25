645c4eb322ffc92cb322f14db9c44559f2d925bc
adamesh <adameshh@gmail.com>
2017-06-24 23:13:19 -0400

    added reservation methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,4 +3,14 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
+  def duration
+    #Assignment: knows about its duration based on checkin and checkout dates
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    #Assignment: knows about its total price
+    (self.listing.price.to_f) * self.duration
+  end
+
 end
