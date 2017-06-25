0e21993642d5596a21ce4df649ed560a29b84177
adamesh <adameshh@gmail.com>
2017-06-25 10:02:26 -0400

    finished res validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,7 +3,8 @@ class Reservation < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
   has_one :review
 
-  validate :checkin_isnt_blank, :checkout_isnt_blank
+  validate :checkin_isnt_blank, :checkout_isnt_blank, :res_own_listing,
+          :checkin_before_checkout, :res_available?
 
   def duration
     #Assignment: knows about its duration based on checkin and checkout dates
@@ -24,6 +25,40 @@ class Reservation < ActiveRecord::Base
   def checkout_isnt_blank
     if self.checkout == nil
       errors.add(:checkout, "can't be blank")
+    end
+  end
+
+  def res_own_listing
+    if self.guest == self.listing.host
+      errors.add(:checkout, "cannot make a reservation on your own listing")
+    end
+  end
+
+  def res_own_listing
+    if self.guest == self.listing.host
+      errors.add(:checkout, "cannot make a reservation on your own listing")
+    end
+  end
+
+  def res_available?
+# validates that a listing is available at checkin before making reservation
+# validates that a listing is available at checkout before making reservation
+# validates that a listing is available at for both checkin and checkout before making reservation
+    if self.checkout == nil || self.checkin == nil
+      nil
+    elsif !self.listing.reservations.all? do |reservation|
+      reservation.checkin > self.checkout ||
+      reservation.checkout < self.checkin
+        errors.add(:checkin, "checkin or checkout timing invalid")
+      end
+    end
+  end
+
+  def checkin_before_checkout
+# validates that checkin is before checkout
+# validates that checkin and checkout dates are not the same
+    if self.checkin.nil? || self.checkout.nil? || self.checkin >= self.checkout
+      errors.add(:checkin, "checkin must be after checkout")
     end
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,17 +2,28 @@ class Review < ActiveRecord::Base
   belongs_to :reservation
   belongs_to :guest, :class_name => "User"
 
-  validate :rating_isnt_blank, :description_isnt_blank
+  validate :rating_valid?, :description_valid?, :reservation_valid?
 
-  def rating_isnt_blank
+  def rating_valid?
     if self.rating == nil
       errors.add(:rating, "can't be blank")
     end
   end
 
-  def description_isnt_blank
+  def description_valid?
     if self.description == nil
       errors.add(:description, "can't be blank")
+    end
+  end
+
+  def reservation_valid?
+    # Assignment: review validations is invalid without an
+    # associated reservation, has been accepted, and checkout has happened
+    # the review they feed us seems valid, I'm confused.
+    if self.reservation == nil ||
+      self.reservation.status != "accepted" ||
+      self.reservation.checkout == nil
+      errors.add(:reservation, "is invalid")
     end
   end
 
