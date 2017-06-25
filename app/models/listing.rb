0eeb294cfb5b321950edb66255126f51228d4daf
adamesh <adameshh@gmail.com>
2017-06-24 22:15:03 -0400

    finished listing methods

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,5 +4,12 @@ class Listing < ActiveRecord::Base
   has_many :reservations
   has_many :reviews, :through => :reservations
   has_many :guests, :class_name => "User", :through => :reservations
-  
+
+  def average_review_rating
+    #Assignment: knows its average ratings from its reviews
+    self.reviews.inject(0) do |sum, review|
+      sum + review[:rating].to_f / reviews.length
+    end
+  end
+
 end
