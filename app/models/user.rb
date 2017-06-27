27e8910df2dbe4fc2821780f68c4c34160d95bcb
adamesh <adameshh@gmail.com>
2017-06-27 10:04:34 -0400

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,14 @@ class User < ActiveRecord::Base
   has_many :reservations, :through => :listings
   has_many :trips, :foreign_key => 'guest_id', :class_name => "Reservation"
   has_many :reviews, :foreign_key => 'guest_id'
+
   has_many :guests, :through => :listings, :foreign_key => 'host_id'
   has_many :host_reviews, :through => :listings, :source => "reviews"
   # The one below doesn't pass
-  has_many :hosts, :through => :trips, :foreign_key => 'guest_id'
+
+  def hosts
+    #guest.trips.first.listing.host
+    self.trips.map {|trip| trip.listing.host}
+  end
 
 end
